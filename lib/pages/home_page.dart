484c8e867bb65a39e0f8bde6b9ad76f93b9d26bd
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-08-13 10:14:54 -0500

    Mods

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,23 @@
+import 'package:codigo5_recipes/widgets/recipe_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   String title = "";
+
   TextEditingController titleController = TextEditingController();
+
   TextEditingController descriptionController = TextEditingController();
+
   TextEditingController imageController = TextEditingController();
+
+  List<Map<String, dynamic>> recipes = [];
 
   @override
   Widget build(BuildContext context) {
@@ -139,7 +150,17 @@ class HomePage extends StatelessWidget {
                   width: double.infinity,
                   height: 48.0,
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Map<String, dynamic> recipe = {
+                        "title": titleController.text,
+                        "description": descriptionController.text,
+                        "image": imageController.text,
+                      };
+
+                      recipes.add(recipe);
+
+                      setState(() {});
+                    },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Color(0xffC0E863),
                       shape: RoundedRectangleBorder(
@@ -172,97 +193,19 @@ class HomePage extends StatelessWidget {
 
                 //Elementos - Recetas registradas
 
-                RecipeItem(
-                  title: "Rocoto Relleno",
-                  description:
-                      "El rocoto relleno es el emblema gastronómico arequipeño por excelencia y también un símbolo del mestizaje cultural del Perú, pues combina insumos originarios de los Andes, así como aquellos traídos por los europeos hace 500 años.",
-                  image:
-                      "https://comeperuano.b-cdn.net/wp-content/uploads/2020/04/receta-rocoto-relleno..jpg",
-                ),
-                RecipeItem(
-                  title: "Ceviche",
-                  description:
-                      "El Ceviche de pescado peruano es el plato tradicional por excelencia en el Perú. Los peruanos lo consideramos nuestro plato bandera y nos sentimos especialmente orgullosos de él.",
-                  image:
-                      "https://cdn0.recetasgratis.net/es/posts/7/4/1/ceviche_peruano_18147_600_square.jpg",
-                ),
+                ...recipes
+                    .map(
+                      (e) => RecipeItem(
+                        title: e["title"],
+                        description: e["description"],
+                        image: e["image"],
+                      ),
+                    )
+                    .toList(),
               ],
             ),
           ),
         ),
-      ),
-    );
-  }
-}
-
-class RecipeItem extends StatelessWidget {
-  String title;
-  String description;
-  String image;
-
-  RecipeItem({
-    required this.title,
-    required this.description,
-    required this.image,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 12),
-      padding: const EdgeInsets.all(14.0),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(16.0),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.07),
-            offset: const Offset(5, 5),
-            blurRadius: 10,
-          ),
-        ],
-      ),
-      // height: 400,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(14.0),
-            child: Image.network(
-              image,
-              height: 230.0,
-              width: double.infinity,
-              fit: BoxFit.cover,
-            ),
-          ),
-          const SizedBox(
-            height: 8.0,
-          ),
-          Text(
-            title,
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-              fontSize: 18.0,
-              fontWeight: FontWeight.w600,
-              color: Color(0xff2f2f2f),
-            ),
-          ),
-          const SizedBox(
-            height: 4.0,
-          ),
-          Text(
-            description,
-            maxLines: 3,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-              fontSize: 14.0,
-              fontWeight: FontWeight.w400,
-              color: Color(0xff2f2f2f).withOpacity(0.8),
-            ),
-          ),
-        ],
       ),
     );
   }
---
+++ lib/widgets/recipe_item.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class RecipeItem extends StatelessWidget {
+  String title;
+  String description;
+  String image;
+
+  RecipeItem({
+    required this.title,
+    required this.description,
+    required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 12),
+      padding: const EdgeInsets.all(14.0),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.07),
+            offset: const Offset(5, 5),
+            blurRadius: 10,
+          ),
+        ],
+      ),
+      // height: 400,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(14.0),
+            child: Image.network(
+              image,
+              height: 230.0,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(
+            height: 8.0,
+          ),
+          Text(
+            title,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.w600,
+              color: Color(0xff2f2f2f),
+            ),
+          ),
+          const SizedBox(
+            height: 4.0,
+          ),
+          Text(
+            description,
+            maxLines: 3,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontSize: 14.0,
+              fontWeight: FontWeight.w400,
+              color: Color(0xff2f2f2f).withOpacity(0.8),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
